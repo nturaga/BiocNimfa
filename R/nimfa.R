23e44b1136787745324dc6688200ea4541023b78
Nitesh Turaga <nitesh.turaga@gmail.com>
2018-06-13 12:50:10 -0400

    Initial commit to package BiocNimfa
    This package interfaces with the nimfa library in python with the use of reticulate.

---
+++ R/nimfa.R
@@ -0,0 +1,33 @@
+#' Load nimfa library in R
+#'
+#' @importFrom reticulate use_virtualenv
+#' @importFrom reticulate import
+#' @importFrom reticulate virtualenv_create
+#' @importFrom reticulate virtualenv_install
+#' @examples
+#' nimfa = load_nimfa()
+#' v <- nimfa$examples$medulloblastoma$read(normalize=TRUE)
+#' lsnmf <- nimfa$Lsnmf(v, seed="random_vcol", rank=50L, max_iter=100L)
+#' lsnmf_fit <- lsnmf()
+#' print(lsnmf_fit$fit$rss())
+#' @export
+load_nimfa<-
+    function(virtualenv = "BiocNimfa")
+    {
+        ## Test if package is present
+        if(file.exists(virtualenv)){
+            ## Use virtualenv
+            reticulate::use_virtualenv(
+                virtualenv = file.path("~/.virtualenvs/", virtualenv))
+        } else {
+            ## Create and install package, but package needs all dependencies
+            reticulate::virtualenv_install(envname = virtualenv,
+                                           packages = c("nose","matplotlib",
+                                                        "numpy","scipy", "nimfa"))
+            reticulate::use_virtualenv(file.path("~/.virtualenvs", virtualenv))
+        }
+        ## import
+        reticulate::import("nimfa")
+    }
+
+
---
+++ tests/testthat/test_nimfa.R
@@ -0,0 +1,15 @@
+context("BiocNimfa")
+
+test_that("nimfa loads and performs basic test", {
+    nimfa = load_nimfa()
+    v <- nimfa$examples$medulloblastoma$read(normalize=TRUE)
+    lsnmf <- nimfa$Lsnmf(v, seed='random_vcol', rank=50L, max_iter=100L)
+    lsnmf_fit <- lsnmf()
+    # expect_equal(lsnmf_fit$fit$rss(), 0.2668)
+    # expect_equal(lsnmf_fit$fit$evar(), 0.9997)
+    # expect_equal(lsnmf_fit$distance(metric='kl'), 38.8744)
+    expect_equal(round(lsnmf_fit$fit$sparseness()[[1]][1], 1), 0.7)
+    expect_equal(round(lsnmf_fit$fit$sparseness()[[2]][1], 1), 0.8)
+})
+
+
