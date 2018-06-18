de7b1877f56afe844f5c032182b640eef8fa021f
Martin Morgan <martin.morgan@roswellpark.org>
2018-06-18 05:38:23 -0400

    formatting
    - Bioc standard version number
    - wrapped Authors@R
    - 'standard' indentation and spacing in R/*
    - use 'macOS' for Mac OS.

--- R/nimfa.R
+++ R/nimfa.R
@@ -15,27 +15,29 @@
 #' @export
 install_nimfa <-
     function(virtualenv = "BiocNimfa")
-    {
-        is_windows <- identical(.Platform$OS.type, "windows")
-        is_osx <- Sys.info()["sysname"] == "Darwin"
-        is_linux <- identical(tolower(Sys.info()[["sysname"]]), "linux")
-        if (!is_windows && !is_osx && !is_linux) {
-            stop("Unable to install TensorFlow on this platform. ",
-                 "Binary installation is available for Windows, OS X, and Linux")
-        }
-        ## Test if package is present
-        if(file.exists(virtualenv)){
-            ## Use virtualenv
-            reticulate::use_virtualenv(
-                virtualenv = file.path("~/.virtualenvs", virtualenv))
-        } else {
-            ## Create and install package, but package needs all dependencies
-            reticulate::virtualenv_install(envname = virtualenv,
-                                           packages = c("nimfa"))
-            reticulate::use_virtualenv(file.path("~/.virtualenvs", virtualenv))
-        }
-        ## import
-        reticulate::import("nimfa")
+{
+    is_windows <- identical(.Platform$OS.type, "windows")
+    is_osx <- Sys.info()["sysname"] == "Darwin"
+    is_linux <- identical(tolower(Sys.info()[["sysname"]]), "linux")
+    if (!is_windows && !is_osx && !is_linux) {
+        stop(
+            "Unable to install 'Nimfa' on this platform. ",
+            "Binary installation is available for Windows, macOS, and Linux"
+        )
     }
-
-
+    ## Test if package is present
+    if (file.exists(virtualenv)) {
+        ## Use virtualenv
+        reticulate::use_virtualenv(
+            virtualenv = file.path("~/.virtualenvs", virtualenv)
+        )
+    } else {
+        ## Create and install package, but package needs all dependencies
+        reticulate::virtualenv_install(
+            envname = virtualenv, packages = c("nimfa")
+        )
+        reticulate::use_virtualenv(file.path("~/.virtualenvs", virtualenv))
+    }
+    ## import
+    reticulate::import("nimfa")
+}
